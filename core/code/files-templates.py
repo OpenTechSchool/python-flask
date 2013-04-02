1fab5affd0dc077e7a1814f9ea2cfe75ebcbb71a
Steven Farlie <steven.farlie@gmail.com>
2013-04-02 20:57:10 +1100

    Fixed template path

--- core/code/files-templates.py
+++ core/code/files-templates.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('forms-templates.html')
+    return render_template('files-templates.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
